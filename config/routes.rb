94473cb2faefc1173d8f54c2b413d994cfe2c394
zeemai <zee.hao.mai@gmail.com>
2020-08-14 11:03:54 +0100

    u

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [:index, :new, :create, :show] do
+  root to: 'cocktails#index'
+  resources :cocktails, only: [:new, :create, :show] do
     resources :doses, only: [:new, :create]
   end
 
